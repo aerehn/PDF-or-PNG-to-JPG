8f1c388a25abb926f2b344120d69de5a5024e70d
Atte Rehnbäck <aerehn@utu.fi>
2022-02-21 18:07:56 +0200

    name conversion added

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from PIL import Image
 import os
 import cv2
 from pdf2image import convert_from_path
+import pandas as pd
+import openpyxl
 cwd = os.getcwd()
 
 print(cwd)
 
 files = os.listdir()
-
 print(files)
-
+name_conversion_table=pd.read_excel(cwd+"/export_grid.xlsx")
+print(name_conversion_table)
+old_names=list(name_conversion_table["vanhatilausnumero"])
+new_names=list(name_conversion_table["sku"])
 target_path = cwd + "/target"
 source_path = cwd + "/source"
 source_files = os.listdir(source_path)
@@ -38,13 +42,23 @@ def turn_to_jpg(source_path,target_path):
         cv2.imwrite(target_path+"/"+file[:-3]+"jpg", new_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
         #cv2.imwrite(target_path+"/"+file[:-3]+"jpg", new_img)
 
+        index_of_new_name = old_names.index(file[:-4]) if file[:-4] in old_names else None
+        if index_of_new_name != None:
+            new_name=new_names[index_of_new_name]
+            cv2.imwrite(target_path+"/"+new_name+".jpg", new_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        else:
+            cv2.imwrite(target_path+"/"+file[:-4]+".jpg", new_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
     for file in pdf_files:
         images = convert_from_path(source_path+"/"+file)
-        
-        for i in range(len(images)):
-        
+        print(file[:-4])
+        index_of_new_name = old_names.index(file[:-4]) if file[:-4] in old_names else None
+        if index_of_new_name != None:
+            new_name=new_names[index_of_new_name]
             # Save pages as images in the pdf
-            images[i].save(target_path+"/"+file[:-3]+"jpg", 'JPEG')
+            images[0].save(target_path+"/"+new_name+".jpg", 'JPEG')
+        else:
+            images[0].save(target_path+"/"+file[:-4]+".jpg", 'JPEG')
 
     files = os.listdir(source_path)
     subdirectories = []
